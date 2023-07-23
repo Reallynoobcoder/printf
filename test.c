36124b2bd11286bd0cffa5415f6e7380aea31764
Reallynoobcoder <133228964+Reallynoobcoder@users.noreply.github.com>
2023-07-23 16:14:07 +0100

    Delete test.c

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-int main ()
-{
-  int result =
-    _printf ("Hello, %s! This is %c testing the %%printf function.\n", "user",
-	     'A');
-  printf ("Total characters printed: %d\n", result);
-  return 0;
-}
