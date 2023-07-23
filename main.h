0f400b21a006d265aaa29ead8b2238082b121a02
Elimali hamza <elimal.hamza@gmail.com>
2023-07-23 12:22:05 -0700

    header file

--- main.h
+++ main.h
@@ -6,12 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
+
 
 int _printf(const char *format, ...);
-void print_char(va_list args);
-void print_string(va_list args);
-void print_percent(va_list args);
-void print_unknown(va_list args);
 
 #endif
