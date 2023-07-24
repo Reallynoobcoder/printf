5d844fbda5257fbb5e3c693d16b25b9d10ed2c68
Elimali hamza <elimal.hamza@gmail.com>
2023-07-24 08:14:21 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,28 +1,13 @@
 #include "main.h"
 
-int _printf(const char *format, ...)
+int process_format(const char *format, conver_t *funct_list, int size, va_list args)
 {
     int i, j, r_val;
     int result = 0;
 
-    conver_t funct_list[] = {
-        {"c", p_char},
-        {"s", p_string},
-        {"%", p_percent},
-        
-    };
-int size = (sizeof(funct_list) / sizeof(funct_list[0]));
-    va_list args;
-
-    if (format == NULL)
-        return (-1);
-
-    va_start(args, format);
-
     for (i = 0; format[i] != '\0'; i++)
     {
         if (format[i] == '%')
-        
         {
             for (j = 0; j < size; j++)
             {
@@ -30,15 +15,12 @@ int size = (sizeof(funct_list) / sizeof(funct_list[0]));
                 {
                     r_val = funct_list[j].f(args);
                     if (r_val == -1)
-                    {
-                        va_end(args);
-                        return (-1);
-                    }
+                        return -1;
                     result += r_val;
                     break;
                 }
             }
-            if (size == j && format[i + 1] != ' ')
+            if (funct_list[j].sym == NULL && format[i + 1] != ' ')
             {
                 if (format[i + 1] != '\0')
                 {
@@ -48,8 +30,7 @@ int size = (sizeof(funct_list) / sizeof(funct_list[0]));
                 }
                 else
                 {
-                    va_end(args);
-                    return (-1);
+                    return -1;
                 }
             }
             i = i + 1;
@@ -61,6 +42,32 @@ int size = (sizeof(funct_list) / sizeof(funct_list[0]));
         }
     }
 
+    return result;
+}
+
+int _printf(const char *format, ...)
+{
+    int result = 0;
+
+    conver_t funct_list[] = {
+        {"c", p_char},
+        {"s", p_string},
+        {"%", p_percent},
+    };
+    int size = sizeof(funct_list) / sizeof(funct_list[0]);
+
+    va_list args;
+    va_start(args, format);
+
+    if (format == NULL)
+    {
+        va_end(args);
+        return -1;
+    }
+
+    result = process_format(format, funct_list, size, args);
+
     va_end(args);
     return result;
 }
+
