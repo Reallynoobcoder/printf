5db6877bd6573d0e0ee9c5be300e9714e4a8ec11
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-24 04:16:44 -0700

    adding struct in main.h

--- main.h
+++ main.h
@@ -8,8 +8,20 @@
 #include <unistd.h>
 #include <stddef.h>
 
-int _printf(const char *format, ...);
+struct convert
+{
+    char *sym;
+    int (*f)(va_list);
+};
+typedef struct convert conver_t;
 
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int parser(const char *format, conver_t funct_list[], va_list args);
+int p_char(va_list);
+int p_string(va_list args);
+int p_percent(va_list);
 #define ERROR -1
 
 #endif
