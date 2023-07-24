8a82c3cd8bc8aed017ea629cdd0150cca1b4ca21
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-24 07:05:34 -0700

    combien fun

--- _printf.c
+++ _printf.c
@@ -1,22 +1,40 @@
 #include "main.h"
 
-int parser(const char *format, conver_t funct_list[], va_list args)
+int _printf(const char *format, ...)
 {
-    int i, j, r_val, printed_chars;
+    int i, j, r_val;
+    int result = 0;
 
-    printed_chars = 0;
+    conver_t funct_list[] = {
+        {"c", p_char},
+        {"s", p_string},
+        {"%", p_percent},
+        
+    };
+int size = (sizeof(funct_list) / sizeof(funct_list[0]));
+    va_list args;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+
     for (i = 0; format[i] != '\0'; i++)
     {
-        if (format[i] == '%') 
+        if (format[i] == '%')
+        
         {
-            for (j = 0; funct_list[j].sym != NULL; j++)
+            for (j = 0; j < size; j++)
             {
                 if (format[i + 1] == funct_list[j].sym[0])
                 {
                     r_val = funct_list[j].f(args);
                     if (r_val == -1)
+                    {
+                        va_end(args);
                         return (-1);
-                    printed_chars += r_val;
+                    }
+                    result += r_val;
                     break;
                 }
             }
@@ -26,42 +44,23 @@ int parser(const char *format, conver_t funct_list[], va_list args)
                 {
                     _putchar(format[i]);
                     _putchar(format[i + 1]);
-                    printed_chars = printed_chars + 2;
+                    result += 2;
                 }
                 else
+                {
+                    va_end(args);
                     return (-1);
+                }
             }
             i = i + 1;
         }
         else
         {
             _putchar(format[i]);
-            printed_chars++;
+            result++;
         }
     }
-    return (printed_chars);
-}
-int _printf(const char *format, ...)
-{
-    int printed_chars;
 
-    conver_t funct_list[] =    {
-        {"c", p_char},
-        {"s", p_string},
-        {"%", p_percent},
-        {NULL, NULL}
-    };
-
-    va_list args;
-
-
-    if (format == NULL)
-        return (-1);
-
-    va_start(args, format);
-
-    printed_chars = parser(format, funct_list, args);
     va_end(args);
-
-    return (printed_chars);
+    return result;
 }
