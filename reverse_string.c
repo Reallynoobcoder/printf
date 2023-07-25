4c17e4d88d85f43c7cda4d602b8f6ef08da27280
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-25 12:27:37 -0700

    correcting rev

---
+++ reverse_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+char *str = va_arg(args, char*);
+int i = 0;
+int str_len = 0;
+
+while (str[i])
+    i++;
+
+i--;
+
+while (i >= 0)
+{
+    _putchar(str[i]);
+    str_len++;
+    i--;
+}
+
+return str_len;
+
