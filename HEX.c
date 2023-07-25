e5fc04558225af357dc379bfd1b60e2af99233e1
Elimali hamza <elimal.hamza@gmail.com>
2023-07-25 10:28:10 -0700

    betty

--- HEX.c
+++ HEX.c
@@ -1,35 +1,48 @@
 #include "main.h"
+
+/**
+ * print_hexaup - Convert unsigned int to hexadecimal and print in uppercase.
+ *
+ * @n: The unsigned int to be converted and printed.
+ *
+ * Return: The number of characters printed.
+*/
 int print_hexaup(unsigned int n)
 {
-    int i, j, rem, counter = 0;
-    unsigned int num_copy = n;
-    char *hex_ud;
+	int i, j, rem, counter = 0;
+	unsigned int num_copy = n;
+	char *hex_ud;
 
-    if (n == 0)
-        return (_putchar('0'));
+	if (n == 0)
+		return (_putchar('0'));
 
-    for (counter = 0; num_copy != 0; counter++)
-        num_copy /= 16;
+	for (counter = 0; num_copy != 0; counter++)
+		num_copy /= 16;
 
-    hex_ud = malloc(counter);
-    for (i = 0; n != 0; i++)
-    {
-        rem = n % 16;
-        hex_ud[i] = rem < 10 ? rem + '0' : rem + 'A' - 10;
-        n /= 16;
-    }
-    for (j = i - 1; j >= 0; j--)
-        _putchar(hex_ud[j]);
+	hex_ud = malloc(counter);
+	for (i = 0; n != 0; i++)
+	{
+		rem = n % 16;
+		hex_ud[i] = rem < 10 ? rem + '0' : rem + 'A' - 10;
+		n /= 16;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(hex_ud[j]);
 
-    free(hex_ud);
-    return (counter);
+	free(hex_ud);
+	return (counter);
 }
 
-
+/**
+ * print_HEX - Convert unsigned int to hexadecimal and print in uppercase.
+ *
+ * @args: The va_list containing the unsigned int to be converted and printed.
+ *
+ * Return: The number of characters printed.
+*/
 int print_HEX(va_list args)
 {
-    unsigned int num = va_arg(args, unsigned int);
+	unsigned int num = va_arg(args, unsigned int);
 
-    return print_hexaup(num);
+	return (print_hexaup(num));
 }
-
--- hex.c
+++ hex.c
@@ -1,33 +1,53 @@
 #include "main.h"
+
+/**
+ * print_hexalow - Convert unsigned int to lowercase hexadecimal and print.
+ *
+ * @n: The unsigned int to be converted and printed.
+ *
+ * Return: The number of characters printed.
+ */
+
 int print_hexalow(unsigned int n)
 {
-    int i, j, rem, counter;
-    unsigned int num_copy = n;
-    char *hex_d;
+	int i, j, rem, counter;
+	unsigned int num_copy = n;
+	char *hex_d;
 
-    if (n == 0)
-        return (_putchar('0'));
+	if (n == 0)
+		return (_putchar('0'));
 
-    for (counter = 0; num_copy != 0; counter++)
-        num_copy /= 16;
+	for (counter = 0; num_copy != 0; counter++)
+		num_copy /= 16;
 
-    hex_d = malloc(counter);
-    for (i = 0; n != 0; i++)
-    {
-        rem = 0;
-        rem = n % 16;
-        if (rem < 10)
-            hex_d[i] = rem + '0';
-        else
-            hex_d[i] = rem + 'W';
-        n /= 16;
-    }
-    for (j = i - 1; j >= 0; j--)
-        _putchar(hex_d[j]);
-    free(hex_d);
-    return (counter);
+	hex_d = malloc(counter);
+	for (i = 0; n != 0; i++)
+	{
+		rem = 0;
+		rem = n % 16;
+		if (rem < 10)
+			hex_d[i] = rem + '0';
+		else
+			hex_d[i] = rem + 'W';
+		n /= 16;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(hex_d[j]);
+	free(hex_d);
+	return (counter);
 }
-int print_hex(va_list args) {
-    unsigned int num = va_arg(args, unsigned int);
-    return print_hexalow(num);
+
+/**
+ * print_hex - Convert unsigned int to hexadecimal and print in lowercase.
+ *
+ * @args: The va_list containing the unsigned int to be converted and printed.
+ *
+ * Return: The number of characters printed.
+ */
+
+int print_hex(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+
+	return (print_hexalow(num));
 }
