1b3ac4b303ac9d19afaad3882f943b5ef7e3984b
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-25 12:46:17 -0700

    rev

--- reverse_string.c
+++ reverse_string.c
@@ -3,16 +3,16 @@ int printf_reverse(va_list args)
 {
 	char *str = va_arg(args, char*);
 	int i = 0;
-	int str_len = 0;
+	int len_str = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 		i++;
 	i--;
 	while (i >= 0)
 	{
 		_putchar(str[i]);
-		str_len++;
+		len_str++;
 		i--;
 	}
-	return (str_len);
+	return (len_str);
 }
