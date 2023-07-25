40da721b4f755b855918d5b4bfdc19aa73bd133a
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-25 08:35:05 -0700

    .

--- HEX.c
+++ HEX.c
@@ -1,38 +1,33 @@
 #include "main.h"
 int print_hexaup(unsigned int n)
 {
-	int i, j, remainder, count = 0;
-	unsigned int num_copy = n;
-	char *hexadecimal;
+    int i, j, remainder, count = 0;
+    unsigned int num_copy = n;
+    char *hexadecimal;
 
-	if (n == 0)
-		return (_putchar('0'));
-	while (num_copy != 0)
-	{
-		num_copy /= 16;
-		count++;
-	}
-	hexadecimal = malloc(count);
-	for (i = 0; n != 0; i++)
-	{
-		remainder = 0;
-		remainder = n % 16;
-		if (remainder < 10)
-		{
-			hexadecimal[i] = remainder + '0';
-		}
-		else
-		{
-			hexadecimal[i] = remainder + '7';
-		}
-		n /= 16;
-	}
-	for (j = i - 1; j >= 0; j--)
-		_putchar(hexadecimal[j]);
+    if (n == 0)
+        return (_putchar('0'));
 
-	free(hexadecimal);
-	return (count);
+    while (num_copy != 0)
+    {
+        num_copy /= 16;
+        count++;
+    }
+
+    hexadecimal = malloc(count);
+    for (i = 0; n != 0; i++)
+    {
+        remainder = n % 16;
+        hexadecimal[i] = remainder < 10 ? remainder + '0' : remainder + 'A' - 10;
+        n /= 16;
+    }
+    for (j = i - 1; j >= 0; j--)
+        _putchar(hexadecimal[j]);
+
+    free(hexadecimal);
+    return (count);
 }
+
 
 int print_HEX(va_list args)
 {
